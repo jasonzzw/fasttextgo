7bbc4f2ea5840c6481c3a428f883dd0b8cbdf39d
Zhao Zhenwei <zhaozw@garena.com>
2018-06-05 20:45:35 +0800

    support multiple models loaded in the same process

--- fasttext.go
+++ fasttext.go
@@ -2,8 +2,8 @@ package fasttextgo
 
 // #cgo LDFLAGS: -L${SRCDIR} -lfasttext -lstdc++ -lm
 // #include <stdlib.h>
-// void load_model(char *path);
-// int predict(char *query, float *prob, char **buf, int *count, int k, int buf_sz);
+// void load_model(char *name, char *path);
+// int predict(char* name, char *query, float *prob, char **buf, int *count, int k, int buf_sz);
 import "C"
 import (
 	"errors"
@@ -11,12 +11,18 @@ import (
 )
 
 // LoadModel - load FastText model
-func LoadModel(path string) {
-	C.load_model(C.CString(path))
+func LoadModel(name, path string) {
+	p1 := C.CString(name)
+	p2 := C.CString(path)
+
+	C.load_model(p1, p2)
+
+	C.free(unsafe.Pointer(p1))
+	C.free(unsafe.Pointer(p2))
 }
 
 // Predict - predict, return the topN predicted label and their corresponding probability
-func Predict(sentence string, topN int) (map[string]float32, error) {
+func Predict(name, sentence string, topN int) (map[string]float32, error) {
 	result := make(map[string]float32)
 
 	cprob := make([]C.float, topN, topN)
@@ -26,9 +32,10 @@ func Predict(sentence string, topN int) (map[string]float32, error) {
 		buf[i] = (*C.char)(C.calloc(64, 1))
 	}
 
+	np := C.CString(name)
 	data := C.CString(sentence)
-	ret := C.predict(data, &cprob[0], &buf[0], &resultCnt, C.int(topN), 64)
 
+	ret := C.predict(np, data, &cprob[0], &buf[0], &resultCnt, C.int(topN), 64)
 	if ret != 0 {
 		return result, errors.New("error in prediction")
 	} else {
@@ -38,6 +45,7 @@ func Predict(sentence string, topN int) (map[string]float32, error) {
 	}
 	//free the memory used by C
 	C.free(unsafe.Pointer(data))
+	C.free(unsafe.Pointer(np))
 	for i := 0; i < topN; i++ {
 		C.free(unsafe.Pointer(buf[i]))
 	}
--- src/fasttext_wrapper.cc
+++ src/fasttext_wrapper.cc
@@ -4,6 +4,7 @@
 #include "real.h"
 #include <streambuf>
 #include <cstring>
+#include <map>
 
 extern "C" {
 
@@ -14,33 +15,33 @@ struct membuf : std::streambuf
     }
 };
 
-fasttext::FastText g_fasttext_model;
-bool g_fasttext_initialized = false;
+std::map<std::string, fasttext::FastText*> g_fasttext_model;
 
-void load_model(char *path) {
-  if (!g_fasttext_initialized) {
-    g_fasttext_model.loadModel(std::string(path));
-    g_fasttext_initialized = true;
-  }
+void load_model(char *name, char *path) {
+	fasttext::FastText *model=new fasttext::FastText();
+	model->loadModel(std::string(path));
+	g_fasttext_model[std::string(name)]=model;
 }
 
 //get top k result
-int predict(char *query, float *prob, char **buf, int *count, int k, int buf_sz) {
+int predict(char* name, char *query, float *prob, char **buf, int *count, int k, int buf_sz) {
   membuf sbuf(query, query + strlen(query));
   std::istream in(&sbuf);
 
   std::vector<std::pair<fasttext::real, std::string>> predictions;
-
-  g_fasttext_model.predict(in, k, predictions);
-
-  int i=0;
-  for (auto it = predictions.cbegin(); it != predictions.cend() && i<k; it++) {
-    *(prob+i) = (float)exp(it->first);
-    strncpy(*(buf+i), it->second.c_str(), buf_sz);
-	i++;
+  try {
+		  g_fasttext_model.at(std::string(name))->predict(in, k, predictions);
+		
+		  int i=0;
+		  for (auto it = predictions.cbegin(); it != predictions.cend() && i<k; it++) {
+		    *(prob+i) = (float)exp(it->first);
+		    strncpy(*(buf+i), it->second.c_str(), buf_sz);
+			i++;
+		  }
+		  *count=i;
+		  return 0;
+  } catch (const std::exception& e) { 
+		return 1;
   }
-  *count=i;
-  return 0;
 }
-
 }
